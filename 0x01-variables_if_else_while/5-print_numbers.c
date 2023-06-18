cba8833f809d49e83cc355ed889737c8d0d6ed0a
mohamedzaghloull <mmzaghlool076@gmail.com>
2023-06-18 09:33:29 -0700

    g

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,10 @@ int main(void)
 {
 	int n = 0;
 
-        while (n <= 9)
-        {
-                printf("%d", n);
-                        n++;
-        }
-
-        return (0);
+	while (n <= 9)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
 }
