1af32f03a103307209e43e93187e9b756b99258b
mohamedzaghloull <mmzaghlool076@gmail.com>
2023-06-18 07:47:25 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 97;
+
+	while (ch <= 122)
+{
+	putchar(ch);
+	ch++;
+
+}
+
+	putchar('\n');
+	return (0);
+}
